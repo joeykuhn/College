fc4a505d47e8c662d9f8b7f93bf2182c1c8a903c
joeykuhn <jck244@mst.edu>
2017-02-09 13:52:09 -0600

    Program compiles, Seg Faults

--- 1510CS/2hw/arraylist.h
+++ 1510CS/2hw/arraylist.h
@@ -1,8 +1,8 @@
 /* @file: arraylist.h
  * Definition of templated class ArrayList
  * Implementation of the List ADT
- * @C - Galaxy Express Softworks 
- * 
+ * @C - Galaxy Express Softworks
+ *
  * Version 17.1.0
  */
 
@@ -15,21 +15,21 @@
   /                 \
   | ArrayList Class |
   \_________________/
-   
-    Purpose: Implements a List ADT using a dynamically allocated array to 
-        store the elements of the list 
 
-    Error Handling: Whenever a function is given invalid parameter values 
+    Purpose: Implements a List ADT using a dynamically allocated array to
+        store the elements of the list
+
+    Error Handling: Whenever a function is given invalid parameter values
         the message "!-- ERROR : PANIC in ARRAYLIST!!" and an explanation
-        is displayed. The list remains unchanged. 
-        If such function returns a value this value is the dummy member 
+        is displayed. The list remains unchanged.
+        If such function returns a value this value is the dummy member
         object 'm_errobj'.
 */
 
 
 // --------------- Forward Declarations ---------------
 template <typename T>                  // the ArrayList class
-class ArrayList;                       
+class ArrayList;
 
 template <typename T>                 // a friend function for output
 std::ostream& operator<< (std::ostream& out, const ArrayList<T>& alist);
@@ -39,8 +39,8 @@ std::ostream& operator<< (std::ostream& out, const ArrayList<T>& alist);
 // ---------------  Class ArrayList Declarations ---------------
 template <typename T>
 class ArrayList
-{ 
-private:  
+{
+private:
   int m_size;                          // current number of elements
   int m_max;                           // maximum capacity of array m_data
   T* m_data;                           // array to store the elements
@@ -48,20 +48,20 @@ private:
   T m_errobj;                          // dummy object to return in case of error
 
 
-  /* 
-   * ----- Auxiliary Functions ----- 
+  /*
+   * ----- Auxiliary Functions -----
    */
 
   // Purpose: Increases size of storage array
   // Postconditions: a new array of size m_max*2 is allocated,
-  //     and m_data is copied to it. 
+  //     and m_data is copied to it.
   //     m_max is set to the size of this new storage array
   void grow();
 
 
   // Purpose: Decreases size of storage array
   // Postconditions: a new array of size m_max/2 is allocated,
-  //     and m_data is copied to it. 
+  //     and m_data is copied to it.
   //     m_max is set to the size of this new storage array
   void shrink();
 
@@ -71,7 +71,7 @@ private:
    */
 public:
   // Purpose: Default Constructor
-  // Postconditions: Current size and maximum size set to 0, 
+  // Postconditions: Current size and maximum size set to 0,
   // and data set to NULL.
   // -- INLINE
   ArrayList(): m_size(0), m_max(0), m_data(NULL) {};
@@ -79,7 +79,7 @@ public:
   // Purpose: Parameterized Constructor
   // Parameters:  s : the requested size of the list
   //              x : the initial values in the list
-  // Postconditions:  the list is constructed as a list of 's' copies of 'x' 
+  // Postconditions:  the list is constructed as a list of 's' copies of 'x'
   ArrayList(int s, const T& x);
 
 
@@ -88,7 +88,7 @@ public:
    */
 
   // Purpose: Destructor
-  // Postconditions: current size and maximum size set to 0, 
+  // Postconditions: current size and maximum size set to 0,
   //                 data deallocated and set to NULL
   ~ArrayList();
 
@@ -104,16 +104,16 @@ public:
   ArrayList(const ArrayList<T>& cpy);
 
 
-  /* 
-   * ----- Basic Accessor Operations ----- 
+  /*
+   * ----- Basic Accessor Operations -----
    */
-  
+
   // Purpose: Accessor function for the number of elements in the list
-  // Returns: number of elements in the list 
+  // Returns: number of elements in the list
   int size() const;
 
   // Purpose: Accessor function for the max capacity of elements
-  // Returns: max capacity of this list 
+  // Returns: max capacity of this list
   // -- INLINE
   // ( used for grading purposes )
   int max() const { return m_max; };
@@ -122,13 +122,13 @@ public:
   // Returns: the first element in the list
   const T& first() const;
 
-  // Purpose: return a read-and-write reference to the element at 
+  // Purpose: return a read-and-write reference to the element at
   // the specified position
   // Parameters: i is 0-based index into the ArrayList
   // Returns: the element at position i in the ArrayList
   T& operator[](int i);
 
-  // Purpose: return a read-only reference to the element at 
+  // Purpose: return a read-only reference to the element at
   // the specified position
   // Parameters: i is 0-based index into the ArrayList
   // Returns: the element at position i in the ArrayList
@@ -144,28 +144,28 @@ public:
 
 
 
-  /* 
-   * ----- Basic Mutator Operations ----- 
+  /*
+   * ----- Basic Mutator Operations -----
    */
 public:
   // Purpose: Clears the ArrayList
-  // Postconditions: current size and maximum size set to 0, 
+  // Postconditions: current size and maximum size set to 0,
   //                 elements are deallocated and data is set to NULL
   void clear();
-  
+
   // Purpose: puts the value x at the end of an ArrayList
   // Parameters: x is value to be added to ArrayList
   // Postconditions: current size is incremented by 1
   //     If max size is reached, the storage wrray is grown.
   void insert_back(const T& x);
-  
+
   // Purpose: puts the value x at the position i in the  ArrayList
   // Parameters: x is value to be added to ArrayList
   //             i is the position to insert x at
   // Postconditions: current size is incremented by 1
   //     If max size is reached, the storage array is grown().
   void insert(const T& x, int i);
-    
+
   // Purpose: Removes the element at index i in the array
   // Parameters: i, the index of the element to remove
   // Postconditions: if the size of the list is greater than 0
@@ -175,8 +175,8 @@ public:
   void remove(int i);
 
 
-  /* 
-   * ----- Complex Mutator Operations ----- 
+  /*
+   * ----- Complex Mutator Operations -----
    */
 
   // Purpose: swaps two elements of the array
@@ -187,7 +187,7 @@ public:
   // Purpose: appends two lists
   // Parameters: alist, a list to appended to the end of 'this' list
   // Postconditions: current size is incremented by size of alist
-  //     the elements of alist are appended to the elements of 'this' 
+  //     the elements of alist are appended to the elements of 'this'
   void append(const ArrayList<T>& alist);
 
   // Purpose: Reverses an ArrayLost
@@ -195,12 +195,12 @@ public:
   void reverse();
 
 
-  /* 
-   * ----- Complex Accessors Operations ----- 
+  /*
+   * ----- Complex Accessors Operations -----
    */
 
   // Purpose: prints the ArrayList
-  friend std::ostream& operator<< <> (std::ostream& out, 
+  friend std::ostream& operator<< <> (std::ostream& out,
                                       const ArrayList<T>& alist);
 };
 
@@ -210,17 +210,17 @@ public:
 // This is the function to pretty print the lists
 // -----------------  Friend Function to Print  -----------------
 // Purpose: prints an ArrayList
-template <typename T>                 
+template <typename T>
 std::ostream& operator<< (std::ostream& out, const ArrayList<T>& alist)
 {
   out << "[ ";
   for (int i=0; i < alist.m_size; i++){
     out << alist.m_data[i] << ", ";
-  } 
+  }
   out << "]";
   return out;
 }
-  
+
 
 #include "arraylist.hpp"
 #endif
--- 1510CS/2hw/arraylist.hpp
+++ 1510CS/2hw/arraylist.hpp
@@ -1,49 +1,41 @@
-/* @file: arraylist.h
- * Definition of templated class ArrayList
- * Implementation of the List ADT
- * @C - Galaxy Express Softworks
- *
- * Version 17.1.0
- */
 
 #include <iostream>
 #include "arraylist.h"
-  // Purpose: Increases size of storage array
-  // Postconditions: a new array of size m_max*2 is allocated,
-  //     and m_data is copied to it.
-  //     m_max is set to the size of this new storage array
+
   template <typename T>
   void ArrayList<T>::grow()
   {
     T * tempArray;
+    if (m_max == 0)
+    {
+      m_max++;
+    }
 
-    tempArray = new T * [m_max*2];
-
+    tempArray = new T  [m_max*2];
+    std::cout << "Before growing, m_size is " << m_size << std::endl;
     for (int i = 0; i < m_size; i++)
+    {
       tempArray[i] = m_data[i];
-
+      std::cout << "Putting " << m_data[i] << " into tempArray" << std::endl;
+    }
     delete[] m_data;
-
+    std::cout << "m_data deleted[]" << std::endl;
     m_data = tempArray;
 
+    std::cout << "m_data = tempArray" << std::endl;
     m_max *= 2;
 
-    delete tempArray;
+    tempArray = NULL;
 
     return;
   }
 
-
-  // Purpose: Decreases size of storage array
-  // Postconditions: a new array of size m_max/2 is allocated,
-  //     and m_data is copied to it.
-  //     m_max is set to the size of this new storage array
   template <typename T>
   void ArrayList<T>::shrink()
   {
-    T * tempArray;
+    T *tempArray;
 
-    tempArray = new T * [m_max/2];
+    tempArray = new T [m_max/2];
 
     for (int i = 0; i < m_size; i++)
       tempArray[i] = m_data[i];
@@ -59,9 +51,6 @@
     return;
   }
 
-  // Purpose: Destructor
-  // Postconditions: current size and maximum size set to 0,
-  //                 data deallocated and set to NULL
   template <typename T>
   ArrayList<T>::~ArrayList()
   {
@@ -71,97 +60,70 @@
     m_data = NULL;
   }
 
-  // Purpose: Operator=, performs a deep copy of 'rhs' into 'this' ArrayList
-  // Parameters: rhs : ArrayList to be copied
-  // Returns: *this
-  // Postconditions: rhs == this
   template <typename T>
   ArrayList<T> & ArrayList<T>::operator=(const ArrayList<T>& rhs)
   {
-    for (int i = 0; i < rhs.m_size; i++)
-    {
-      m_data[i] = rhs.m_data[i];
-    }
     m_size = rhs.m_size;
     m_max = rhs.m_max;
+    m_data = new T [rhs.m_max];
+    std::cout << "rhs.m_size before assigning is " << rhs.m_size << std::endl;
+    std::cout << "m_size before assigning is " << m_size << std::endl;
+    for (int i = 0; i < rhs.m_size; i++)
+    {
+      std::cout << "Copying " << rhs.m_data[i] << "." << std::endl;
+      m_data[i] = rhs.m_data[i];
+    }
 
     return (*this);
   }
 
-  // Purpose: Copy Constructor
-  // Parameters: cpy = ArrayList to be copied
-  // Postconditions: current size, maximum size, and elements same as cpy
   template <typename T>
   ArrayList<T>::ArrayList(const ArrayList<T>& cpy)
   {
-    for (int i = 0; i < m_size; i++)
+    for (int i = 0; i < cpy.m_size; i++)
     {
-      cpy.m_data[i] = m_data[1];
+      m_data[i] = cpy.m_data[i];
     }
-    cpy.m_size = m_size;
-    cpy.m_max = m_max;
+    m_size = cpy.m_size;
+    m_max = cpy.m_max;
   }
 
-  // Purpose: Accessor function for the number of elements in the list
-  // Returns: number of elements in the list
   template <typename T>
   int ArrayList<T>::size() const
   {
        return m_size;
   }
 
-  // Purpose: Accessor function for the first element of the list
-  // Returns: the first element in the list
   template<typename T>
   const T& ArrayList<T>::first() const
   {
     return (m_data[0]);
   }
 
-  // Purpose: return a read-and-write reference to the element at
-  // the specified position
-  // Parameters: i is 0-based index into the ArrayList
-  // Returns: the element at position i in the ArrayList
   template <typename T>
   T& ArrayList<T>::operator[](int i)
   {
     return (m_data[i]);
   }
 
-  // Purpose: return a read-only reference to the element at
-  // the specified position
-  // Parameters: i is 0-based index into the ArrayList
-  // Returns: the element at position i in the ArrayList
   template <typename T>
   const T& ArrayList<T>::operator[](int i) const
   {
     return (m_data[i]);
   }
 
-  // Purpose: finds an element in the ArrayList
-  // Parameters: 'x' is value to be found in the ArrayList
-  // Returns: the position of the first occurrance of 'x' in the list,
-  //          or -1  if 'x' is not found.
   template <typename T>
   int ArrayList<T>::find(const T& x) const
   {
     for (int i = 0; i < m_size; i++)
     {
       if (m_data[i] == x)
-        return x;
+        return i;
     }
     return (-1);
 
   }
 
-
-
-
-
-
-  // Purpose: Clears the ArrayList
-  // Postconditions: current size and maximum size set to 0,
-  //                 elements are deallocated and data is set to NULL
   template<typename T>
   void ArrayList<T>::clear()
   {
@@ -173,22 +135,20 @@
     return;
   }
 
-  // Purpose: puts the value x at the end of an ArrayList
-  // Parameters: x is value to be added to ArrayList
-  // Postconditions: current size is incremented by 1
-  //     If max size is reached, the storage wrray is grown.
   template <typename T>
   void ArrayList<T>::insert_back(const T& x)
   {
-    m_data[m_size+1] = x;
+    if (m_size == m_max)
+    {
+      grow();
+
+    }
+
+    m_data[m_size] = x;
+    m_size++;
     return;
   }
 
-  // Purpose: puts the value x at the position i in the  ArrayList
-  // Parameters: x is value to be added to ArrayList
-  //             i is the position to insert x at
-  // Postconditions: current size is incremented by 1
-  //     If max size is reached, the storage array is grown().
   template <typename T>
   void ArrayList<T>::insert(const T& x, int i)
   {
@@ -201,69 +161,67 @@
     return;
   }
 
-  // Purpose: Removes the element at index i in the array
-  // Parameters: i, the index of the element to remove
-  // Postconditions: if the size of the list is greater than 0
-  //     size is decremented by one.
-  //     if the size of the list less than 1/4 the max size,
-  //     the storage array is shrunk.
   template <typename T>
   void ArrayList<T>::remove(int i)
   {
-    m_size--;
     if (m_size < (m_max/4))
     {
       shrink();
     }
-    for (int j = m_size; j != i; j--)
+    for (int j = i; j < (m_size+1); j++)
     {
-      m_data[j] = m_data[j-1];
+      m_data[j] = m_data[j+1];
     }
+    m_data[m_size] = NULL;
+    m_size--;
     return;
   }
 
-  /*
-   * ----- Complex Mutator Operations -----
-   */
-
-  // Purpose: swaps two elements of the array
-  // Parameters: i, k, two positions on the Arraylist
-  // Postconditions: the elements at positions i and k swap positions
-  void swap(int i, int k);
-
-  // Purpose: appends two lists
-  // Parameters: alist, a list to appended to the end of 'this' list
-  // Postconditions: current size is incremented by size of alist
-  //     the elements of alist are appended to the elements of 'this'
-  void append(const ArrayList<T>& alist);
-
-  // Purpose: Reverses an ArrayLost
-  // Postconditions: the elements in the list are now reversed.
-  void reverse();
-
-
-  /*
-   * ----- Complex Accessors Operations -----
-   */
-
-  // Purpose: prints the ArrayList
-  friend std::ostream& operator<< <> (std::ostream& out,
-                                      const ArrayList<T>& alist);
-};
-
-
-
-// BONUS, BONUS, BONUS !!!
-// This is the function to pretty print the lists
-// -----------------  Friend Function to Print  -----------------
-// Purpose: prints an ArrayList
-template <typename T>
-std::ostream& operator<< (std::ostream& out, const ArrayList<T>& alist)
-{
-  out << "[ ";
-  for (int i=0; i < alist.m_size; i++){
-    out << alist.m_data[i] << ", ";
+  template <typename T>
+  void ArrayList<T>::swap(int i, int k)
+  {
+    T temp;
+    temp = m_data[i];
+    m_data[i] = m_data[k];
+    m_data[k] = temp;
+    return;
   }
-  out << "]";
-  return out;
-}
+
+
+  template <typename T>
+  void ArrayList<T>::append(const ArrayList<T>& alist)
+  {
+
+    for (int i = 0; i < alist.m_data; i++)
+    {
+
+        if (m_size == m_max)
+        {
+          grow();
+        }
+
+        insert_back(alist.m_data[i]);
+
+      }
+
+    return;
+
+  }
+
+
+  template <typename T>
+  void ArrayList<T>::reverse()
+  {
+
+    for (int i = 0; i < m_size/2; i++)
+    {
+
+        T temp;
+        m_data[i] = temp;
+        m_data[i] = m_data[m_size-i];
+        m_data[m_size-i] = temp;
+
+    }
+
+    return;
+  }
